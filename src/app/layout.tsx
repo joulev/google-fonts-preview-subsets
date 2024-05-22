afc719d9bc18bdd2d2a3966eda729a7188052354
Vu Van Dung <me@joulev.dev>
2024-05-22 21:29:23 +0700

    add shadcn/ui
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import "~/app/globals.css";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen bg-zinc-100 font-sans text-zinc-700 antialiased dark:bg-zinc-900 dark:text-zinc-300">
+        {children}
+      </body>
     </html>
   );
 }
