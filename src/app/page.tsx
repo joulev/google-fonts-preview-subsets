f80973d5ba3f1c19d7c32d29364606abeb5f02a3
Vu Van Dung <me@joulev.dev>
2024-05-22 21:31:46 +0700

    reposition the dropdown
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,9 @@
-"use client";
-
-import { Check, ChevronsUpDown } from "lucide-react";
-import { useState } from "react";
-
-import { Button } from "~/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "~/components/ui/command";
-import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
-import { cn } from "~/lib/utils";
-
-const frameworks = [
-  { value: "next.js", label: "Next.js" },
-  { value: "sveltekit", label: "SvelteKit" },
-  { value: "nuxt.js", label: "Nuxt.js" },
-  { value: "remix", label: "Remix" },
-  { value: "astro", label: "Astro" },
-];
+import { Dropdown } from "./dropdown";
 
 export default function Page() {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
-
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {value
-            ? frameworks.find(framework => framework.value === value)?.label
-            : "Select framework..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="Search framework..." />
-          <CommandEmpty>No framework found.</CommandEmpty>
-          <CommandGroup>
-            {frameworks.map(framework => (
-              <CommandItem
-                key={framework.value}
-                value={framework.value}
-                onSelect={currentValue => {
-                  setValue(currentValue === value ? "" : currentValue);
-                  setOpen(false);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === framework.value ? "opacity-100" : "opacity-0",
-                  )}
-                />
-                {framework.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <div className="my-24 flex flex-row justify-center">
+      <Dropdown />
+    </div>
   );
 }
