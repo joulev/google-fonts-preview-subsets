064a0ee8355a680df4ac22a83700443d1fe4a5ce
Vu Van Dung <me@joulev.dev>
2024-05-23 14:11:13 +0700

    static export
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 import { withNextJSRouteTypes } from "nextjs-route-types";
 
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  headers: async () => [
+    {
+      source: "/v1/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+      ],
+    },
+  ],
+};
 
 export default withNextJSRouteTypes(nextConfig);
