b0164df0fe6bd0bb3bad23c8e2a2ce07f8e2f796
Vu Van Dung <me@joulev.dev>
2024-05-23 14:23:43 +0700

    allow requests for resources from all domains
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,6 @@ import { withNextJSRouteTypes } from "nextjs-route-types";
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "export",
-  headers: async () => [
-    {
-      source: "/v1/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-      ],
-    },
-  ],
 };
 
 export default withNextJSRouteTypes(nextConfig);
